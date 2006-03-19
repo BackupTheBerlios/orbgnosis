eba02713585d20660414331ce61e6b712ac76ea3
trs137 <trs137>
2006-03-19 03:33:59 +0000

    Solves Lambert's problem with universal variables.

---
+++ orbgnosis/src/Lambert.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2006 Ted Stodgell. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: Lambert.cpp,v 1.1 2006/03/19 03:33:59 trs137 Exp $
+ *
+ * Contributor(s):  Ted Stodgell <trs137@psu.edu>
+ *
+ * This file contains the first four Stumpff functions C_k(z).
+ * Note that z = alpha * chi^2.  
+ *
+ * Stumpff functions C_k(z) are defined generally by
+ * C_k(z) = [1/k!] - [z / (k+2)!] + [z^2 / (k+4)!] + ...
+ *
+ */
+
+#include <math.h>
+#inclue "Vector.h"
+#include <iostream>
+using namespace std;
+
+/*
+ * Inputs
+ *          mu (double, gravitational parameter)
+ *          R1 (Vector, start position)
+ *          R2 (Vector, final position)
+ *          t  (double, time of flight)
+ */
+
+void
+lambert(double mu, Vector R1, Vector R2, double t)
+{
+    double r1 = R1.norm();
+    double r2 = R2.norm();
+}
+
+int
+main(void) {
+    cout << "Lambert solver tester!\n";
+
+    return 0;
+}
