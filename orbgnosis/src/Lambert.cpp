43769e3b7e137eebe1524abce6311db80b073031
trs137 <trs137>
2006-04-04 23:11:47 +0000

    This SHOULD solve for the N=0 and N=2 multi-rev cases... we shall see...

--- orbgnosis/src/Lambert.cpp
+++ orbgnosis/src/Lambert.cpp
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: Lambert.cpp,v 1.26 2006/04/04 23:04:32 trs137 Exp $
+ * $Id: Lambert.cpp,v 1.27 2006/04/04 23:11:47 trs137 Exp $
  *
  * Contributor(s):  Ted Stodgell <trs137@psu.edu>
  *                  David Vallado <valladodl@worldnet.att.net>
@@ -150,7 +150,7 @@ Lambert::universal (const bool L, const bool MR)
 
         // Testing N=2 multi-multi rev!!!
         Upper = -0.001 + 36.0 * PI * PI;
-        Lower = 0.001 + 4.0 * PI * PI;
+        Lower = 0.001 + 16.0 * PI * PI;
     }else{
         // common single revolution case
         Upper = 4.0 * PI * PI;
